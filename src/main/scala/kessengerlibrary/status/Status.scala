65750968260769433dbac58e38a2d4448ac76542
malyszaryczlowiek <45798727+malyszaryczlowiek@users.noreply.github.com>
2022-07-25 21:08:26 +0200

    Status enum added

---
+++ src/main/scala/kessengerlibrary/status/Status.scala
@@ -0,0 +1,11 @@
+package com.github.malyszaryczlowiek
+package kessengerlibrary.status
+
+enum Status:
+
+  case NotInitialized
+  case Starting
+  case Running
+  case Idle
+  case Error
+  case Terminated
