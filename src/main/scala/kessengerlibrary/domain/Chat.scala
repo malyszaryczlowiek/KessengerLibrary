81a857bb3306dc287d679d1239924fd30e4b45f0
malyszaryczlowiek <45798727+malyszaryczlowiek@users.noreply.github.com>
2022-07-26 18:58:50 +0200

    Reversed chat ordering added.

--- src/main/scala/kessengerlibrary/domain/Chat.scala
+++ src/main/scala/kessengerlibrary/domain/Chat.scala
@@ -7,7 +7,6 @@ import java.time.LocalDateTime
 
 
 /**
- * tutaj zmienić pakiet z messages na domain
  * @param chatId
  * @param chatName
  * @param groupChat
@@ -15,3 +14,11 @@ import java.time.LocalDateTime
  * @param timeOfLastMessage
  */
 case class Chat(chatId: ChatId, chatName: ChatName, groupChat: Boolean, offset: Long, timeOfLastMessage: LocalDateTime)
+
+object Chat:
+
+  given reverseChatOrdering: Ordering[Chat] with
+    override def compare(x: Chat, y: Chat): Int =
+      if x.timeOfLastMessage.isBefore(y.timeOfLastMessage) then 1
+      else if x.timeOfLastMessage.isAfter(y.timeOfLastMessage) then -1
+      else 0
---
+++ src/test/scala/kessengerlibrary/serdes/ChatTests.scala
@@ -0,0 +1,34 @@
+package com.github.malyszaryczlowiek
+package kessengerlibrary.serdes
+
+import kessengerlibrary.domain.Chat
+import kessengerlibrary.domain.Chat.reverseChatOrdering
+
+import java.time.LocalDateTime
+import java.time.temporal.{ChronoUnit, TemporalUnit}
+import java.util.UUID
+
+class ChatTests extends  munit.FunSuite {
+
+  test("Testing chat ordering") {
+
+    val chatId = "Id"
+    val chatName = "name"
+    val groupChat = false
+    val offset = 0
+    val earlierTime = LocalDateTime.now()
+    val laterTime = earlierTime.plus(2L, ChronoUnit.MINUTES)
+
+
+    val earlier = Chat(chatId, chatName, groupChat, offset, earlierTime)
+    val later   = Chat(chatId, chatName, groupChat, offset, laterTime)
+
+    val list = List(earlier, later)
+    val sorted = list.sorted
+
+    assert(sorted == list.reverse, s"is not reversed")
+
+
+  }
+
+}
