0291286a337f939228091e4f9ca2ff98b4dea2f3
malyszaryczlowiek <45798727+malyszaryczlowiek@users.noreply.github.com>
2022-08-25 22:12:43 +0200

    typo

--- src/main/scala/kessengerlibrary/kafka/errors/KafkaError.scala
+++ src/main/scala/kessengerlibrary/kafka/errors/KafkaError.scala
@@ -1,5 +1,4 @@
 package com.github.malyszaryczlowiek
 package kessengerlibrary.kafka.errors
 
-// earlier messages.kafkaErrorUtils
 case class KafkaError(errorStatus: KafkaErrorStatus, description: KafkaErrorMessage)
